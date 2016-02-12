b49a1c2d69cc1e88cf7fd090af55c1edd6168d56
Yousuke Izumi <yo123na0606@gmail.com>
2016-02-12 16:53:00 +0900

    HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+
+/**
+ * @author internous
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("HelloWorld!");
+	}
+
+}
